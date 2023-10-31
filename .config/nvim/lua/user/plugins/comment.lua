a14ec0bdeaceac10663c28fd8f1d3e7a96ddf85f
Salim <salimhamed@gmail.com>
2023-10-31 11:07:49 -0700

    Misc updates from work computer

--- .config/nvim/lua/user/plugins/comment.lua
+++ .config/nvim/lua/user/plugins/comment.lua
@@ -3,15 +3,21 @@ local M = {
   dependencies = {
     { "JoosepAlviste/nvim-ts-context-commentstring" },
   },
-  opts = {
-    hooks = {
-      pre = function()
-        require("ts_context_commentstring.internal").update_commentstring({})
-      end,
-    },
-  },
-  config = function(_, opts)
-    require("mini.comment").setup(opts)
+  config = function(_, _)
+    -- see the following link for information on integrating nvim-ts-context-commentstring with mini.comment
+    -- https://github.com/JoosepAlviste/nvim-ts-context-commentstring/wiki/Integrations
+    local ts_context_commentstring = require("ts_context_commentstring")
+    ts_context_commentstring.setup({
+      enable_autocmd = false,
+    })
+
+    require("mini.comment").setup({
+      options = {
+        custom_commentstring = function()
+          return ts_context_commentstring.calculate_commentstring() or vim.bo.commentstring
+        end,
+      },
+    })
   end,
 }
 
